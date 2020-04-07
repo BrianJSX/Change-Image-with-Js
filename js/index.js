79044bba42e8e22e0c8313d319b196aec19c60ac
onionMit <minhho.technology@gmail.com>
2020-04-07 07:56:18 -0700

    update name product

--- js/index.js
+++ js/index.js
@@ -3,24 +3,25 @@ function changeImg(id){
     arr = [
         {
             id: 0,
-            Name: 'Quần jean 1',
+            Name: 'Quần Jean Kiểu Pháp',
             price: '100000',
             linkImage: '/img/1.jpg'
         },
         {
             id: 1,
-            Name: 'Quần jean 2',
+            Name: 'Quần Jean Kiểu Nhật',
             price: '200000',
             linkImage: '/img/2.jpg'
         },
         {
             id: 2,
-            Name: 'Quần jean 3',
+            Name: 'Quần Jean Kiểu Ý',
             price: '300000',
             linkImage: '/img/3.jpg'
         }
     ];
     let imgProduct = document.getElementById(arr[id].id).getAttribute("src");
     let nameProduct = document.getElementById('nameProduct').innerHTML = arr[id].Name;
+    let priceProduct = document.getElementById('priceProduct').innerHTML = arr[id].price;
     let c = document.getElementById('imgInfo').setAttribute("src",arr[id].linkImage);
 }
