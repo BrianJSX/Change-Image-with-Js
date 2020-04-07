2629ad802cef42b2954ab94aed43f8c259b0d569
onionMit <minhho.technology@gmail.com>
2020-04-07 07:58:26 -0700

    Set Name is name in arr of function changeImg()

--- js/index.js
+++ js/index.js
@@ -3,25 +3,25 @@ function changeImg(id){
     arr = [
         {
             id: 0,
-            Name: 'Quần Jean Kiểu Pháp',
+            name: 'Quần Jean Kiểu Pháp',
             price: '100000',
             linkImage: '/img/1.jpg'
         },
         {
             id: 1,
-            Name: 'Quần Jean Kiểu Nhật',
+            name: 'Quần Jean Kiểu Nhật',
             price: '200000',
             linkImage: '/img/2.jpg'
         },
         {
             id: 2,
-            Name: 'Quần Jean Kiểu Ý',
+            name: 'Quần Jean Kiểu Ý',
             price: '300000',
             linkImage: '/img/3.jpg'
         }
     ];
     let imgProduct = document.getElementById(arr[id].id).getAttribute("src");
-    let nameProduct = document.getElementById('nameProduct').innerHTML = arr[id].Name;
+    let nameProduct = document.getElementById('nameProduct').innerHTML = arr[id].name;
     let priceProduct = document.getElementById('priceProduct').innerHTML = arr[id].price;
     let c = document.getElementById('imgInfo').setAttribute("src",arr[id].linkImage);
 }
